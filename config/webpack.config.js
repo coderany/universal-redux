6d37327721f0dd792a7d0718439a78bc7f596765
Oleg <oleg@one.com>
2016-01-02 14:54:57 +0400

    md loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -43,7 +43,8 @@ module.exports = {
         { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
         { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
         { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+        { test: /\.md$/, loader: 'markdown-with-front-matter' }
       ]
     },
     progress: true,
